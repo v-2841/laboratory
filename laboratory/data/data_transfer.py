609226eb78755ed5ef4993d3c5274655fb7cbfa8
Vitaliy Pavlov <v-2841@yandex.ru>
2023-09-13 17:19:30 +0400

    13.09.23

--- laboratory/data/data_transfer.py
+++ laboratory/data/data_transfer.py
@@ -2,17 +2,24 @@ import os
 
 from datetime import datetime
 from dotenv import load_dotenv
-import MySQLdb
+import psycopg2
 
 
 load_dotenv()
 host = os.getenv('DB_HOST', 'localhost')
-user = os.getenv('USER', 'django_user')
-password = os.getenv('PASSWORD', '')
-database = os.getenv('DB', 'laboratory')
-db = MySQLdb.connect(host=host, user=user, password=password, database=database)
+port = os.getenv('DB_PORT', 5432)
+user = os.getenv('POSTGRES_USER', 'django_user')
+password = os.getenv('POSTGRES_PASSWORD', '')
+database = os.getenv('POSTGRES_DB', 'laboratory')
+db = psycopg2.connect(
+    dbname=database,
+    user=user,
+    password=password,
+    host=host,
+    port=port,
+)
 cursor = db.cursor()
-with open('reagents.txt', 'r') as file:
+with open('reagents.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
     for line in lines:
         data = line.strip().split('\t')
@@ -21,7 +28,7 @@ with open('reagents.txt', 'r') as file:
                 data[i] = None
             elif i == 3 or i == 4:
                 data[i] = datetime.strptime(data[i], "%d.%m.%Y")
-        sql_query = "INSERT INTO reagents_reagent (`index`, name, amount, manufacture_date, expiration_date) VALUES (%s, %s, %s, %s, %s);"
+        sql_query = "INSERT INTO reagents_reagent (index, name, amount, manufacture_date, expiration_date) VALUES (%s, %s, %s, %s, %s);"
         values = (data[0], data[1], data[2], data[3], data[4])
         cursor.execute(sql_query, values)
     db.commit()
--- laboratory/laboratory/settings.py
+++ laboratory/laboratory/settings.py
@@ -100,7 +100,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -112,7 +113,6 @@ INTERNAL_IPS = ['127.0.0.1']
 AUTH_USER_MODEL = 'users.User'
 
 USER_PROFILE_LENGHT = 200
-VIEW_NUM = 10
 
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'laboratories:main'
