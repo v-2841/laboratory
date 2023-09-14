f63dc54cf69b2aeb988314313575a22700db09c7
Vitaliy Pavlov <v-2841@yandex.ru>
2023-09-14 12:59:42 +0400

    14.09.23

--- laboratory/laboratory/settings.py
+++ laboratory/laboratory/settings.py
@@ -100,11 +100,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = '/var/www/laboratory/static/'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/laboratory/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
