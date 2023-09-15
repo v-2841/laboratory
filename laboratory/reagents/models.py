f2c51593247c7d9093fcca0f85dd1fe2f35b5e5a
Vitaliy Pavlov <v-2841@yandex.ru>
2023-09-15 14:59:14 +0400

    15.09.23

--- laboratory/reagents/models.py
+++ laboratory/reagents/models.py
@@ -20,7 +20,6 @@ class Reagent(models.Model):
     )
     name = models.CharField(
         max_length=200,
-        unique=True,
         db_index=True,
         verbose_name='Название реактива',
     )
